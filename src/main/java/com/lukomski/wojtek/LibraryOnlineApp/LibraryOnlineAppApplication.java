122c8560079fc4d0689d3a66b92c7261d140a29f
wojciechlukomski <w.b.lukomski@gmail.com>
2022-03-01 13:30:55 +0100

    adding files

---
+++ src/main/java/com/lukomski/wojtek/LibraryOnlineApp/LibraryOnlineAppApplication.java
@@ -0,0 +1,13 @@
+package com.lukomski.wojtek.LibraryOnlineApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryOnlineAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryOnlineAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lukomski/wojtek/LibraryOnlineApp/LibraryOnlineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukomski.wojtek.LibraryOnlineApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryOnlineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
